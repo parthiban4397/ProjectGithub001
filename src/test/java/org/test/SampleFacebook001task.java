f4641da1bd74adf03b02da5a7cde5df7e2e12e19
parthiban4397 <parthiban.sri4397@gmail.com>
2022-07-20 20:53:13 +0530

    project struct git 2

---
+++ src/test/java/org/test/SampleFacebook001task.java
@@ -0,0 +1,36 @@
+package org.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleFacebook001task {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.facebook.com/");
+		WebElement txtUsername = driver.findElement(By.id("email"));
+		txtUsername.sendKeys("parthiban.sri4397@gmail.com");
+		WebElement txtPassword = driver.findElement(By.name("pass")); 
+		txtPassword.sendKeys("1234567");
+//		WebElement btnLogin = driver.findElement(By.name("login"));
+//		btnLogin.click();
+		System.out.println(txtUsername.getAttribute("value"));
+		
+		
+		WebElement getText1 = driver.findElement(By.xpath("//h2[@class='_8eso']"));
+		System.out.println(getText1.getAttribute("innerText"));
+		System.out.println(getText1.getText());
+		
+		
+	}
+	
+	
+}
